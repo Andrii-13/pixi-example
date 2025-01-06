5849d7a445ff15ee0486c71d3e929f76c770e65a
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2025-01-06 20:57:24 +0200

    hhh

--- src/index.js
+++ src/index.js
@@ -7,3 +7,12 @@
 import './container/index.js'
 //import './tinting/index'
 //import './particle-container/index'
+if (window.__REACT_DEVTOOLS_GLOBAL_HOOK__ && window.__REACT_DEVTOOLS_GLOBAL_HOOK__.renderers) {
+    if (window.__REACT_DEVTOOLS_GLOBAL_HOOK__.renderers.size > 0) {
+      console.log("React DevTools активні");
+    } else {
+      console.log("Немає активних рендерерів");
+    }
+  } else {
+    console.log("React DevTools не знайдено");
+  }
--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,5 @@ import svgLoader from 'vite-plugin-svg-loader';
 
 export default {
   plugins: [svgLoader()],
+  base: '/pixi-example/',
 };
